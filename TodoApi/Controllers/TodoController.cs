200b48e8006976163bf2d2acd5e66dd860d27947
Rob Wilson <rob.wilson@banyanway.com>
2018-05-28 17:58:35 -0400

    Adds initial Web API project and wiring.

---
+++ TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using TodoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            this._context = context;
+
+            if (this._context.TodoItems.Count() <= 0)
+            {
+                this._context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                this._context.SaveChanges();
+            }
+        }
+    }
+}
